8f78db1f2636f225f42ee12b8b22752ee3e460da
Guilherme Henrique Paggi Daros <ghd@certi.org.br>
2025-08-29 20:02:53 -0300

    improve sb::types

--- include/types/StringLiteral.hpp
+++ include/types/StringLiteral.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <string_view>
 
 namespace sb::types {
 
@@ -9,14 +10,6 @@ struct StringLiteral {
   char data[N]{};
 
   consteval StringLiteral(const char (&str)[N]) { std::copy_n(str, N, data); }
-
-  consteval bool operator==(const StringLiteral<N> str) const { return std::equal(str.data, str.data + N, data); }
-
-  template <std::size_t N2>
-  consteval bool operator==(const StringLiteral<N2> s) const {
-    (void)s;
-    return false;
-  }
 
   template <std::size_t N2>
   consteval StringLiteral<N + N2 - 1> operator+(const StringLiteral<N2> str) const {
@@ -29,6 +22,22 @@ struct StringLiteral {
   consteval char operator[](std::size_t n) const { return data[n]; }
 
   consteval std::size_t size() const { return N - 1; }
+
+  consteval auto view() const { return std::string_view(data, size()); }
+
+  template <std::size_t N2>
+  consteval auto operator<=>(const StringLiteral<N2>& other) const {
+    return view() <=> other.view();
+  }
+
+  template <std::size_t N2>
+  consteval bool operator==(const StringLiteral<N2>& other) const {
+    if constexpr (N != N2) {
+      return false;
+    } else {
+      return view() == other.view();
+    }
+  }
 };
 
 template <std::size_t s1, std::size_t s2>
@@ -39,16 +48,6 @@ consteval auto operator+(StringLiteral<s1> fs, const char (&str)[s2]) {
 template <std::size_t s1, std::size_t s2>
 consteval auto operator+(const char (&str)[s2], StringLiteral<s1> fs) {
   return StringLiteral<s2>(str) + fs;
-}
-
-template <std::size_t s1, std::size_t s2>
-consteval auto operator==(StringLiteral<s1> fs, const char (&str)[s2]) {
-  return fs == StringLiteral<s2>(str);
-}
-
-template <std::size_t s1, std::size_t s2>
-consteval auto operator==(const char (&str)[s2], StringLiteral<s1> fs) {
-  return StringLiteral<s2>(str) == fs;
 }
 
 }  // namespace sb::types
