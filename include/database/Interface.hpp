f1fae15e78a784d4d1c2cf5ff10c6ed4daed45fb
Guilherme Henrique Paggi Daros <ghd@certi.org.br>
2025-08-29 21:42:35 -0300

    extracting sb::database common functionality

--- include/database/Interface.hpp
+++ include/database/Interface.hpp
@@ -6,15 +6,15 @@
 
 namespace sb::database {
 
-template <class T>
+template <class T, typename PrimaryKeyType = std::string>
 class Interface {
  public:
   virtual ~Interface() = default;
   virtual auto Create(const T& obj) -> bool = 0;
-  virtual auto Read(const std::string& key) -> std::optional<T> = 0;
+  virtual auto Read(const PrimaryKeyType& key) -> std::optional<T> = 0;
   virtual auto ReadAll() -> std::list<T> = 0;
   virtual auto Update(const T& obj) -> bool = 0;
-  virtual auto Delete(const std::string& key) -> bool = 0;
+  virtual auto Delete(const PrimaryKeyType& key) -> bool = 0;
   virtual auto DeleteAll() -> bool = 0;
 };
 
--- src/database/tables/Tag.cpp
+++ src/database/tables/Tag.cpp
@@ -1,16 +1,20 @@
 #include <database/tables/Tag.hpp>
 #include <string>
 
-// TagDaoImpl Implementation
 namespace sb::database {
+
+const std::string TagDAO::TAG_COLUMN = "tag";
+const std::string TagDAO::TYPE_COLUMN = "tag_type";
+
 TagDAO::TagDAO(Handler& db_handler, std::string table_name)
-    : db_handler_{db_handler}, table_name_{std::move(table_name)} {
+    : BaseDAO<TagDTO, std::string>(db_handler, std::move(table_name)) {
   CreateTable();
 }
 
 void TagDAO::CreateTable() {
   std::lock_guard<std::mutex> lock(db_handler_.get_mutex());
-  std::string sql = "CREATE TABLE IF NOT EXISTS " + table_name_ + " (tag TEXT PRIMARY KEY, tag_type INTEGER);";
+  std::string sql = "CREATE TABLE IF NOT EXISTS " + table_name_ + " (" + TAG_COLUMN + " TEXT PRIMARY KEY, " +
+                    TYPE_COLUMN + " INTEGER);";
   char* err_msg = nullptr;
   int rc = sqlite3_exec(db_handler_.get(), sql.c_str(), 0, 0, &err_msg);
   if (rc != SQLITE_OK) {
@@ -20,128 +24,46 @@ void TagDAO::CreateTable() {
 }
 
 auto TagDAO::Create(const TagDTO& obj) -> bool {
-  std::lock_guard<std::mutex> lock(db_handler_.get_mutex());
-  std::string sql = "INSERT INTO " + table_name_ + " (tag, tag_type) VALUES (?, ?);";
-  sqlite3_stmt* stmt = nullptr;
-  int rc = sqlite3_prepare_v2(db_handler_.get(), sql.c_str(), -1, &stmt, 0);
-  if (rc != SQLITE_OK) {
-    Database::Error() << "Failed to prepare statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return false;
-  }
-
-  sqlite3_bind_text(stmt, 1, obj.tag().c_str(), -1, SQLITE_STATIC);
-  sqlite3_bind_int(stmt, 2, obj.type());
-
-  rc = sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
-
-  if (rc != SQLITE_DONE) {
-    Database::Error() << "Failed to execute statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return false;
-  }
-  return true;
+  std::string sql = "INSERT INTO " + table_name_ + " (" + TAG_COLUMN + ", " + TYPE_COLUMN + ") VALUES (?, ?);";
+  return execute_statement(sql, [&](sqlite3_stmt* stmt) {
+    sqlite3_bind_text(stmt, 1, obj.tag().c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 2, obj.type());
+  });
 }
 
 auto TagDAO::Read(const std::string& tag) -> std::optional<TagDTO> {
-  std::lock_guard<std::mutex> lock(db_handler_.get_mutex());
-  std::string sql = "SELECT tag, tag_type FROM " + table_name_ + " WHERE tag = ?;";
-  sqlite3_stmt* stmt = nullptr;
-  int rc = sqlite3_prepare_v2(db_handler_.get(), sql.c_str(), -1, &stmt, 0);
-  if (rc != SQLITE_OK) {
-    Database::Error() << "Failed to prepare statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return std::nullopt;
-  }
-
-  sqlite3_bind_text(stmt, 1, tag.c_str(), -1, SQLITE_STATIC);
-
-  rc = sqlite3_step(stmt);
-  if (rc == SQLITE_ROW) {
-    std::string read_tag = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
-    int read_type = sqlite3_column_int(stmt, 1);
-    sqlite3_finalize(stmt);
-    return TagDTO(read_tag, read_type);
-  }
-
-  sqlite3_finalize(stmt);
-  return std::nullopt;
+  std::string sql =
+      "SELECT " + TAG_COLUMN + ", " + TYPE_COLUMN + " FROM " + table_name_ + " WHERE " + TAG_COLUMN + " = ?;";
+  return query_single(
+      sql, [&](sqlite3_stmt* stmt) { sqlite3_bind_text(stmt, 1, tag.c_str(), -1, SQLITE_STATIC); },
+      [&](sqlite3_stmt* stmt) {
+        std::string read_tag = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+        int read_type = sqlite3_column_int(stmt, 1);
+        return TagDTO(read_tag, read_type);
+      });
 }
 
 auto TagDAO::ReadAll() -> std::list<TagDTO> {
-  std::lock_guard<std::mutex> lock(db_handler_.get_mutex());
-  std::list<TagDTO> results;
-  std::string sql = "SELECT tag, tag_type FROM " + table_name_ + ";";
-  sqlite3_stmt* stmt = nullptr;
-  int rc = sqlite3_prepare_v2(db_handler_.get(), sql.c_str(), -1, &stmt, 0);
-  if (rc != SQLITE_OK) {
-    Database::Error() << "Failed to prepare statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return results;
-  }
-
-  while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+  std::string sql = "SELECT " + TAG_COLUMN + ", " + TYPE_COLUMN + " FROM " + table_name_ + ";";
+  return query_all(sql, [&](sqlite3_stmt* stmt) {
     std::string read_tag = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
     int read_type = sqlite3_column_int(stmt, 1);
-    results.emplace_back(read_tag, read_type);
-  }
-
-  sqlite3_finalize(stmt);
-  return results;
+    return TagDTO(read_tag, read_type);
+  });
 }
 
 auto TagDAO::Update(const TagDTO& obj) -> bool {
-  std::lock_guard<std::mutex> lock(db_handler_.get_mutex());
-  std::string sql = "UPDATE " + table_name_ + " SET tag_type = ? WHERE tag = ?;";
-  sqlite3_stmt* stmt = nullptr;
-  int rc = sqlite3_prepare_v2(db_handler_.get(), sql.c_str(), -1, &stmt, 0);
-  if (rc != SQLITE_OK) {
-    Database::Error() << "Failed to prepare statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return false;
-  }
-
-  sqlite3_bind_int(stmt, 1, obj.type());
-  sqlite3_bind_text(stmt, 2, obj.tag().c_str(), -1, SQLITE_STATIC);
-
-  rc = sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
-
-  if (rc != SQLITE_DONE) {
-    Database::Error() << "Failed to execute statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return false;
-  }
-  return sqlite3_changes(db_handler_.get()) > 0;
+  std::string sql = "UPDATE " + table_name_ + " SET " + TYPE_COLUMN + " = ? WHERE " + TAG_COLUMN + " = ?;";
+  return execute_statement(sql, [&](sqlite3_stmt* stmt) {
+    sqlite3_bind_int(stmt, 1, obj.type());
+    sqlite3_bind_text(stmt, 2, obj.tag().c_str(), -1, SQLITE_STATIC);
+  });
 }
 
 auto TagDAO::Delete(const std::string& tag) -> bool {
-  std::lock_guard<std::mutex> lock(db_handler_.get_mutex());
-  std::string sql = "DELETE FROM " + table_name_ + " WHERE tag = ?;";
-  sqlite3_stmt* stmt = nullptr;
-  int rc = sqlite3_prepare_v2(db_handler_.get(), sql.c_str(), -1, &stmt, 0);
-  if (rc != SQLITE_OK) {
-    Database::Error() << "Failed to prepare statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return false;
-  }
-
-  sqlite3_bind_text(stmt, 1, tag.c_str(), -1, SQLITE_STATIC);
-
-  rc = sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
-
-  if (rc != SQLITE_DONE) {
-    Database::Error() << "Failed to execute statement: " << sqlite3_errmsg(db_handler_.get()) << std::endl;
-    return false;
-  }
-  return sqlite3_changes(db_handler_.get()) > 0;
+  std::string sql = "DELETE FROM " + table_name_ + " WHERE " + TAG_COLUMN + " = ?;";
+  return execute_statement(sql,
+                           [&](sqlite3_stmt* stmt) { sqlite3_bind_text(stmt, 1, tag.c_str(), -1, SQLITE_STATIC); });
 }
 
-auto TagDAO::DeleteAll() -> bool {
-  std::lock_guard<std::mutex> lock(db_handler_.get_mutex());
-  std::string sql = "DELETE FROM " + table_name_ + ";";
-  char* err_msg = nullptr;
-  int rc = sqlite3_exec(db_handler_.get(), sql.c_str(), 0, 0, &err_msg);
-  if (rc != SQLITE_OK) {
-    Database::Error() << "Failed to execute statement: " << err_msg << std::endl;
-    sqlite3_free(err_msg);
-    return false;
-  }
-  return true;
-}
 }  // namespace sb::database
