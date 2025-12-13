6e0a5e128533912f891bbe9262b337f97705aea9
Guilherme Daros <gh5777.gh@gmail.com>
2025-12-13 01:41:05 -0300

    refactoring typefinder to use concepts

--- include/logger/Meta.hpp
+++ include/logger/Meta.hpp
@@ -1,22 +1,41 @@
 #pragma once
 
-#include <type_traits>
+#include <concepts>
 
 namespace sb::logger::meta {
 
-template <typename Base, template <typename> typename Concept, typename... Types>
+// Helper concept to verify that the Predicate is a trait-like struct
+// with a boolean `value` member for a given type.
+template <template <typename> class Trait, typename T>
+concept PredicateTrait = requires {
+  { Trait<T>::value } -> std::convertible_to<bool>;
+};
+
+template <typename Base, template <typename> typename Predicate, typename... Types>
 struct TypeFinder;
 
-template <typename Base, template <typename> typename Concept, typename First, typename... Rest>
-struct TypeFinder<Base, Concept, First, Rest...> {
-  using type = std::conditional_t<Concept<First>::value, First, typename TypeFinder<Base, Concept, Rest...>::type>;
+// Recursive case: The first type in the list satisfies the predicate.
+// The `requires` clause checks this condition.
+template <typename Base, template <typename> typename Predicate, typename First, typename... Rest>
+  requires PredicateTrait<Predicate, First> && Predicate<First>::value
+struct TypeFinder<Base, Predicate, First, Rest...> {
+  using type = First;
 };
 
-template <typename Base, template <typename> typename Concept>
-struct TypeFinder<Base, Concept> {
-  using type = Base::Default;
+// Recursive case: The first type does not satisfy the predicate.
+// The `requires` clause checks this, and we recurse on the rest of the types.
+template <typename Base, template <typename> typename Predicate, typename First, typename... Rest>
+  requires PredicateTrait<Predicate, First> && (!Predicate<First>::value)
+struct TypeFinder<Base, Predicate, First, Rest...> {
+  using type = typename TypeFinder<Base, Predicate, Rest...>::type;
 };
 
-template <typename Base, template <typename> typename Concept, typename... Types>
-using TypeFinder_t = typename TypeFinder<Base, Concept, Types...>::type;
+// Base case: The list of types is empty, so we fall back to the default type.
+template <typename Base, template <typename> typename Predicate>
+struct TypeFinder<Base, Predicate> {
+  using type = typename Base::Default;
+};
+
+template <typename Base, template <typename> typename Predicate, typename... Types>
+using TypeFinder_t = typename TypeFinder<Base, Predicate, Types...>::type;
 }  // namespace sb::logger::meta
