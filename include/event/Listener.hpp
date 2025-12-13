fa038820ead632d9fa7d8d8dd66a5b04fb6c0256
Guilherme Daros <gh5777.gh@gmail.com>
2025-12-13 01:41:06 -0300

    fix race conditions on futures

--- include/event/Listener.hpp
+++ include/event/Listener.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <cstdint>
 #include <future>
+#include <mutex>
 #include <unordered_map>
+#include <vector>
 
 namespace sb::event {
 
@@ -9,36 +12,45 @@ class Notifier;
 
 template <typename T>
 class Listener {
-  using List = std::unordered_map<std::size_t, T*>;
+  using List = std::unordered_map<std::uintptr_t, T*>;
 
  public:
   static auto Notify(auto... args) -> void {
-    auto run = [](auto... args) -> void {
-      for (auto listener : notify_list()) {
-        (*std::get<1>(listener))(args...);
-      }
-    };
-
-    future_ = std::async(std::launch::async, run, args...);
+    std::lock_guard<std::mutex> lock(mtx_);
+    for (auto const& [id, listener] : notify_list()) {
+      futures_.push_back(std::async(std::launch::async, [listener, args...]() { (*listener)(args...); }));
+    }
   }
 
   static auto Wait() -> void {
-    if (future_.valid()) {
-      future_.wait();
+    std::vector<std::future<void>> local_futures;
+    {
+      std::lock_guard<std::mutex> lock(mtx_);
+      local_futures.swap(futures_);
+    }
+
+    for (auto& f : local_futures) {
+      if (f.valid()) {
+        f.wait();
+      }
     }
   }
 
   static auto notify_list() -> List& { return notify_list_; }
 
  private:
-  static std::future<void> future_;
   static List notify_list_;
+  static std::vector<std::future<void>> futures_;
+  static std::mutex mtx_;
 };
 
 template <typename T>
 Listener<T>::List Listener<T>::notify_list_;
 
 template <typename T>
-std::future<void> Listener<T>::future_;
+std::vector<std::future<void>> Listener<T>::futures_;
+
+template <typename T>
+std::mutex Listener<T>::mtx_;
 
 }  // namespace sb::event
